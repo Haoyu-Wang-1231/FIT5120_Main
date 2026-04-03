2cd8459e3e2ac7a513e592f0d00d0b748b5b9c76
Haoyu-Wang-1231 <hwan0328@student.monash.edu>
2026-04-03 20:48:54 +1100

    initial springboot project.

---
+++ backend/src/test/java/fit5120/monash/edu/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fit5120.monash.edu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
